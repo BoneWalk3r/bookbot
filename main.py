a01127cd43e2d320531e74763fed4f0ab43a9a2a
BoneWalk3r <thecrimsonknight1230@proton.me>
2025-12-18 18:28:37 -0500

    Created 'get_book_text' function

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(path_to_book):
+    with open(path_to_book) as book:
+        text = book.read()
+    return text
+    
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+main()
