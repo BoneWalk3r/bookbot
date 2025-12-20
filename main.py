22f101e2c77526062c6d876f877511117ed12666
BoneWalk3r <thecrimsonknight1230@proton.me>
2025-12-20 13:24:22 -0500

    Added 'count_characters' function to stats.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from stats import count_words, get_book_text
+from stats import get_book_text, count_words, count_characters
 
 def main():
     print(count_words(get_book_text("./books/frankenstein.txt")))
+    print(count_characters(get_book_text("./books/frankenstein.txt")))
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,13 @@ def get_book_text(path_to_book):
     
 def count_words(text_to_be_counted):
     return f"Found {len(text_to_be_counted.split())} total words"
+
+def count_characters(text_to_be_counted):
+    simplified_text = text_to_be_counted.lower()
+    results = {}
+    for character in simplified_text:
+        if character in results:
+            results[character] = results[character] + 1
+        else:
+            results[character] = 1
+    return results
